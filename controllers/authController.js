20ae797bf3c8a45f2725ba1be53143bcecc6876e
IvanM <mahankovage@gmail.com>
2022-07-15 14:32:42 +0300

    consolelog

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,6 +50,7 @@ exports.registration = async (req, res) => {
     const collections = await Collection.findAll({ where: { userId }, order: [['id']] });
 
     req.session.user = { id: userId, login: user.login };
+    console.log(req.session);
     renderFrontWithId(HomeCollect, { id: user.id, collections }, res);
   } catch (error) {
     console.log('error: ', error.message);
